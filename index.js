c3f5e8dd914e7ef1d435b3d9a2f5a603166d64de
Trevor Dunlap <tjdunlap1899@gmail.com>
2023-01-05 12:55:20 -0500

    commit tree f1fee9785db7ebcc686aec2709e30bf65da7f499

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const LeastFavoriteCustomer = "some initial value";
+
+function changeLeastFavoriteCustomer() {
+     LeastFavoriteCustomer = 'possibly bob';
+}
